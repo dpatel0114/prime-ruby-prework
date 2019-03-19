7f4f39686dc2cb4768b70d59fefa78cf382c1389
Dhara Patel <1991dharapatel@gmail.com>
2019-03-19 02:39:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,14 @@
 # Add  code here!
 def prime?(n)
+  for i in range(2..n)
+        if n % i == 0 
+            return false
+    end
+    return true 
+  
+  
+  
+  
   if n <= 1
         return false
   end 
